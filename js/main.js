3b4b2b09c201fb5fb97b4dcd6c441da0995ae68a
Douglas Marquez <dmarquez@satvicos1.com>
2023-08-01 12:54:09 -0400

    darkmode

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 var typed = new Typed('.typed', {
-    strings: ["We Create &amp; Optimize"],
+    strings: ['<span class="light">We Create &amp; Optimize</span>'],
     typeSpeed: 90,
     backSpeed: 90,
     loop:false,
     startDelay: 1000,
     cursorChar: '_',
 
+
 });
+
+var icon = document.getElementById("icon");
+    icon.onclick = function(){
+        document.body.classList.toggle("dark-theme");
+        if(document.body.classList.contains("dark-theme")){
+            icon.src = "./img/sun.png";
+        }else {
+            icon.src="./img/moon.png";
+        }
+          if (document.body.classList.contains("dark-theme")) {
+    icon2.src = "./img/logo_dark.png";
+  } else {
+    icon2.src = "./img/logo.png";
+  }
+};
+    
+ 
+
