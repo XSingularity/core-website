2df32f94cb9abb66526e34a576bca36ca6df206e
Douglas Marquez <dmarquez@satvicos1.com>
2023-08-01 11:24:29 -0400

    changed animation

--- js/main.js
+++ js/main.js
@@ -2,5 +2,8 @@ var typed = new Typed('.typed', {
     strings: ["We Create &amp; Optimize"],
     typeSpeed: 90,
     backSpeed: 90,
-    loop: true
+    loop:false,
+    startDelay: 1000,
+    cursorChar: '_',
+
 });
