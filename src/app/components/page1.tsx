0cb9ff78cb83baa7fbc885a382041e2fa14161b7
Douglas Márquez <douglas09marquez@gmail.com>
2023-08-04 15:48:41 -0400

    updates

--- src/app/components/page1.tsx
+++ src/app/components/page1.tsx
@@ -3,18 +3,54 @@ import React from 'react'
 const Page1 = () => {
   return (
     <section className="text-gray-600 body-font">
-  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+  <div className="container mx-auto flex px-5 py-29 md:flex-row flex-col items-center">
     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 className="title-font sm:text-3xl text-3xl mb-4 font-medium text-gray-900">We Create & Optimize_
+      <h1 className="title-font sm:text-5xl text-3xl mb-4 font-medium text-gray-900">We Create & Optimize_
       </h1>
-      <br />
-      <div className="flex justify-center">
+      
+      <div>
+      <ul className="pl-1 pt-6 market-text-lg list-disc text-gray-900">
+      <nav className="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
+          <a>
+            <span className="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" className="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.
+          </a>
+          <a>
+            <span className="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" className="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>Contrary to popular belief, Lorem Ipsum is not simply random text.
+          </a>
+          <a>
+            <span className="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" className="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
+          </a>
+          <a>
+            <span className="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" className="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+          </a>
+          
+        </nav>
+</ul>
+      </div>
+      
+      <div className="flex justify-center py-8">
         
-        <button className="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-500 rounded text-lg">Button</button>
+        <button className="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-500 rounded text-lg">Contact</button>
       </div>
     </div>
     <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-    <img src="/cube.png" alt="xSingularity" />
+    <img src="/xsing.png" alt="xSingularity" />
     </div>
   </div>
 </section>
