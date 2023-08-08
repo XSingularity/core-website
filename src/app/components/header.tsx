b93e5687d28c797edb088fcfde782ae87dbf3a9e
Douglas Márquez <douglas09marquez@gmail.com>
2023-08-08 16:33:19 -0400

    fixes

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -8,10 +8,10 @@ const Header = () => {
     <img className="test" src="/logo1.png" alt="" />
     <br />
     <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-gray-300">Home</a>
-      <a className="mr-5 hover:text-gray-300">Our team</a>
-      <a className="mr-5 hover:text-gray-300">How we work</a>
-      <a className="mr-5 hover:text-gray-300">Contact</a>
+      <a className="ease-in duration-300 mr-5 hover:text-gray-300">Home</a>
+      <a className="ease-in duration-300 mr-5 hover:text-gray-300">Our team</a>
+      <a className="ease-in duration-300 mr-5 hover:text-gray-300">How we work</a>
+      <a className="ease-in duration-300 mr-5 hover:text-gray-300">Contact</a>
     </nav>
     <br />
     <img width={"30px"} src="/moon.png" alt="" />
