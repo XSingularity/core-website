9207240d7d04b17c3e3b2eb913ce262124572f8b
Douglas Márquez <douglas09marquez@gmail.com>
2023-08-22 16:41:01 -0400

    gradient

--- src/app/components/intro.tsx
+++ src/app/components/intro.tsx
@@ -11,7 +11,7 @@ const Intro = () => {
          <img src="/c3.png" className='absolute -z-10 top-80 blur-sm left-1/2 rotate-45 w-[70px]' alt="" />
          <img src="/c4.png" className='absolute top-10 right-1/2 rotate-45  w-[100px]' alt="" />
 
-          <h1 className="title-font sm:text-5xl lg:text-4xl text-3xl mb-4 font-semibold text-gray-900">We Create & Optimize_
+          <h1 className="title-font sm:text-5xl lg:text-4xl text-3xl mb-4 font-semibold bg-gradient-to-r from-gray-600 to-gray-900 text-transparent bg-clip-text">We Create & Optimize_
           </h1>
 
           <div>
@@ -24,6 +24,7 @@ const Intro = () => {
                     </svg>
                   </span>There are many variations of passages of Lorem Ipsum available.
                 </a>
+                
                 <a>
                   <span className="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="w-3 h-3" viewBox="0 0 24 24">
