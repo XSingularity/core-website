1fac91e364bc0e5b697183acb62365abd3263bef
Douglas Márquez <douglas09marquez@gmail.com>
2023-08-31 14:08:29 -0400

    components changes for scroll

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {Link} from 'react-scroll'
+import {BrowserRouter} from 'react-router-dom'
+import {useRef} from 'react'
+
 
 const Header = () => {
   return (
     <header className="text-gray-900 font-sans font-light ">
       <div className=" container mx-auto flex flex-wrap p-6 xl:px-40 2xl:px-40 lg:px-7 md:px-7 flex-col md:flex-row items-center ">
 
-        <img className="w-32" src="/logo1.png" alt="" />
+        <a href="/"><img className="w-32" src="/logo1.png" alt="" /></a>
         <br />
         
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 font-normal md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-          <a href="#" className="ease-in duration-300 mr-5 hover:text-blue-500">Home</a>
           <a href="#" className="ease-in duration-300 mr-5 hover:text-blue-500">Our team</a>
           <a href="#" className="ease-in duration-300 mr-5 hover:text-blue-500">How we work</a>
           <a href="#" className="ease-in duration-300 mr-5 hover:text-blue-500">Contact</a>
