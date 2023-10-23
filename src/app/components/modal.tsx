fc179bd76b4a59c3087d9383a449c531f5d8b6ba
Douglas Márquez <douglas09marquez@gmail.com>
2023-10-23 14:25:16 -0400

    FAQ

--- src/app/components/modal.tsx
+++ src/app/components/modal.tsx
@@ -1,5 +1,23 @@
     import React from "react";
     import { TypeAnimation } from 'react-type-animation';
+    import { useState } from "react";
+
+    const TituloDesplegable = ({ titulo, contenido }: { titulo: string, contenido: string }) => {
+        const [abierto, setAbierto] = useState(false);
+      
+        const toggleDesplegar = () => {
+          setAbierto(!abierto);
+        };
+      
+        return (
+          <div>
+            <h1 className="text-xl font-normal text-gray-700 " onClick={toggleDesplegar}> 
+              {titulo} <span className=" cursor-pointer hover:text-gray-400">{abierto ? '▲' : '▼'}</span>
+            </h1>
+            {abierto && <p>{contenido}</p>}
+          </div>
+        );
+      };
 
     const Modal = ({ isVisible, onClose } : {isVisible : boolean , onClose:() => void } ) => {
         if (!isVisible) return null;
@@ -16,16 +34,16 @@
         onClick={handleClose}
         //Cuando le de click al fondo negro blurred se va a cerrar el modal!
         >
-            <div className='flex flex-col'>
+            <div className='flex flex-col '>
             <button className="text-white text-xl place-self-end" onClick={() => onClose()}>X</button>
             
-            <div className="bg-white drop-shadow-lg rounded-lg p-20 z-100 " >
+            <div className=" bg-white drop-shadow-lg rounded-lg p-20 z-100 xl:w-[900px] md:w-[900px] sm:w-[500px] " >
             <TypeAnimation 
-      sequence={[
+            sequence={[
        
-        "FAQ's", // Para editar el pointer ve a hoja de estilos global.css y edita "type"
-        1000
-      ]}
+            "FAQ's", // Para editar el pointer ve a hoja de estilos global.css y edita "type"
+            1000
+            ]}
       wrapper="span"
       speed={30}
       className={'type justify-center items-center text-2xl flex mb-10 text-sans font-semibold'}
@@ -33,9 +51,14 @@
       repeat={Infinity}
     />
                 
-                <p>askldjklasjdklasjkldklaslaskjdklasjldkjaskldjklasjdlkasjdkljaskldjkasjdklajldkajklsd</p> 
-                <p>asjkdlasjdklasdasldjkasdasldjklasjdas</p>          
-                <p>expandible cada titulo y ya</p>
+      <div className="grid gap-5 ">
+      <TituloDesplegable titulo="Este sera el titulo 1 de las FAQs" 
+      contenido="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable." />
+      <TituloDesplegable titulo="Este sera el titulo 2 de las FAQs" contenido="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable." />
+      <TituloDesplegable titulo="Este sera el titulo 3 de las FAQs" contenido="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable." />
+     
+
+    </div>
             </div>
             </div>
         </div>
