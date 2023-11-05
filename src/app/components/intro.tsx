5b1e78ed824a4eeae7854a0cb05bfaca79ef3cc0
Douglas Márquez <douglas09marquez@gmail.com>
2023-11-05 17:50:33 -0400

    justify

--- src/app/components/intro.tsx
+++ src/app/components/intro.tsx
@@ -39,7 +39,7 @@ const Intro = () => {
             <ul className="pl-1 pt-6 text-md market-text-lg list-disc text-gray-900">
               {/* edit puntos, tamano letra etc*/}
               <nav className="flex text-base font-sans text-gray-600 flex-col sm:items-start sm:text-left text-center items-center -mb-5 space-y-2.5">
-                <a>
+                <a className="text-justify">
                   <span className="bg-indigo-100 text-blue-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center ">
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="w-3 h-3 " viewBox="0 0 24 24">
                       <path d="M20 6L9 17l-5-5 "></path>
@@ -47,21 +47,20 @@ const Intro = () => {
                   </span>We specialize in crafting high-quality, innovative solutions that bring your projects to life with precision and creativity.
                 </a>
 
-                <a>
+                <a className="text-justify">
                   <span className="bg-indigo-100 text-blue-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="w-3 h-3" viewBox="0 0 24 24">
                       <path d="M20 6L9 17l-5-5"></path>
                     </svg>
                   </span>Empowering businesses through our strategies and visionary approach.
                 </a>
-                <a>
+                <a className="text-justify">
                   <span className="bg-indigo-100 text-blue-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="w-3 h-3" viewBox="0 0 24 24">
                       <path d="M20 6L9 17l-5-5"></path>
                     </svg>
                   </span>Partner with us to drive high-quality results and achieve lasting success in your business endeavors.
                 </a>
-
               </nav>
             </ul>
           </div>
