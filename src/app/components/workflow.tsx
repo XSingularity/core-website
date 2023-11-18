bd4e225e4b2fea276ca404397471699204fcd6a7
Omar <omarcorporated@gmail.com>
2023-11-17 19:46:10 -0600

    final version 0.0.1

--- src/app/components/workflow.tsx
+++ src/app/components/workflow.tsx
@@ -7,12 +7,6 @@ import { TablerRocket } from './svg/Rocket';
 import { Support } from './svg/Support';
 import { debounce } from 'lodash';
 
-const TitleComponent = ({ title }:{title:string} ) => (
-  <div className="absolute top-0 left-1/2 transform -translate-x-1/2 text-white font-bold text-center w-[200px] -mt-12">
-    {title}
-  </div>
-);
-
 const Block = (props: InjectedViewportProps<HTMLDivElement>) => {
   const { forwardedRef } = props;
   return (
@@ -39,106 +33,7 @@ const Workflow = (props: InjectedViewportProps<HTMLDivElement>) => {
     };
   }, []);
 
-<<<<<<< HEAD
-  // Define una función para renderizar el div en función del tamaño de la ventana
-  const renderDivBasedOnWindowSize = () => {
-    if (windowSize < 1024) {
-      // Para tamaños pequeños.....
-      return <div className="">
-        <div className="h-full w-full bg-gradient-to-r from-blue-500 to-blue-700 py-40 justify-center sm:py-12 font-sans drop-shadow-xl">
-          <div className="text-center w-full sm:mb-30 md:mb-30 lg:mb-40 xl:mb-40 mt-10 text-white ">
-            <h1 className="text-3xl font-bold mb-4">WORKFLOW</h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-              We offer a comprehensive range of services, from project management to
-              design and development, to ensure the success of your projects and
-              applications.
-            </p>
-          </div>
-          <div className=" items-center  justify-center mx-auto drop-shadow-lg ">
-            <div className="relative">
-              <div className="w-2 h-full bg-white absolute top-1/2 transform -translate-y-1/2 opacity-0  "></div>
-              {/* ESTOY QUITANDO LA LINEA */}
-              {/* LINEA */}
-              <div className="flex-column justify-between ">
-                {tooltips.map((text, index) => (
-                  <div
-                    key={index}
-                    className="relative transition duration-1000 items-center text-center top-5 cursor-pointer "
-                    onMouseEnter={() => handleMouseEnter(index)}
-                    onMouseLeave={handleMouseLeave}
-                  >
-                    {activeTooltip === index && (
-                      <div className="absolute left-1/2 transform -translate-x-1/2 bg-white text-black p-2 rounded-lg shadow w-[300px] h-[116px] z-10 flex items-center justify-center  ">
-                        {tooltips[index]}
-                      </div>
-                    )}
-                    {/* INFO */}
-                    <div className="w-[90px] h-[90px] bg-white rounded-full cursor-pointer left-[50%] transform mx-auto my-20"></div>
-                    <ViewportBlock onEnterViewport={() => setActiveTooltip(index)} onLeaveViewport={() => setActiveTooltip(null)} />
-                    {/* CIRCULO */}
-
-                    <div className="absolute top-[50%] left-[50%] transform translate-y-[-50%] translate-x-[-50%] cursor-pointer">
-                      {icons[index]}
-                    </div>
-                    <div className="absolute top-0 left-1/2 transform -translate-x-1/2 text-white font-bold text-center -mt-10">
-                      {titles[index]}
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div></div>;
-    } else {
-      // Para tamaños grandes.........
-      return <div className="">
-      
-      <div className="h-[700px] bg-gradient-to-r from-blue-500 to-blue-700 py-40 justify-center sm:py-12 font-sans drop-shadow-xl">
-        <div className="text-center w-full mb-40 mt-10 text-white">
-          <h1 className="text-3xl font-bold mb-4">WORKFLOW</h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            We offer a comprehensive range of services, from project management to
-            design and development, to ensure the success of your projects and
-            applications.
-          </p>
-          </div>
-            <div className="w-[900px] items-center justify-center mx-auto drop-shadow-lg">
-              <div className="relative">
-                <div className="h-2 bg-white w-full absolute top-1/2 transform -translate-y-1/2"></div>
-                <div className="flex justify-between">
-                  {tooltips.map((text, index) => (
-                    <div
-                      key={index}
-                      className="relative hover:scale-[1.1] transition duration-1000"
-                      onMouseEnter={() => handleMouseEnter(index)}
-                      onMouseLeave={handleMouseLeave}
-                    >
-                      <div className="w-[90px] h-[90px] bg-white rounded-full cursor-pointer"></div>
-                      {activeTooltip === index && (
-                        <div className="absolute left-1/2 transform -translate-x-1/2 bg-white text-black p-2 rounded-lg shadow text-center mt-10 w-[200px]">
-                          {text}
-                        </div>
-                      )}
-                      <div className="absolute top-[50%] left-[50%] transform translate-y-[-50%] translate-x-[-50%] cursor-pointer">
-                        {icons[index]}
-                        <TitleComponent title={titles[index]} /> 
-                      </div>
-                     
-                    </div>
-                  
-                ))}
-                
-              </div>
-            </div>
-          </div>
-        </div></div>;
-    }
-  };
-
-  const [tooltips, setTooltips] = useState([
-=======
   const tooltipsData = [
->>>>>>> 6a5cf24dc4268d0cb96dab2fcad90856d160efaf
     "This is where we analyze the requirements of your project, ensuring that we fully understand your needs and goals.",
     "Our experts transform the requirements into solid and efficient code, using industry best practices.",
     "The code undergoes unit testing and quality controls to ensure that your software operates flawlessly.",
@@ -147,7 +42,7 @@ const Workflow = (props: InjectedViewportProps<HTMLDivElement>) => {
   ];
 
   const [tooltips, setTooltips] = useState<string[]>(tooltipsData);
-  const [activeTooltip, setActiveTooltip] = useState<number | null>(null);
+  const [activeTooltip, setActiveTooltip] = useState<number | null>(0);
 
   const icons = [
     <TablerCheckupList />,
@@ -186,9 +81,9 @@ const Workflow = (props: InjectedViewportProps<HTMLDivElement>) => {
                 applications.
               </p>
             </div>
-            <div className=" items-center  justify-center mx-auto drop-shadow-lg ">
+            <div className=" items-center justify-center mx-auto drop-shadow-lg">
               <div className="relative">
-                <div className="w-2 h-full bg-white absolute top-1/2 transform -translate-y-1/2 opacity-0  "></div>
+                <div className="w-2 h-full bg-white absolute top-1/2 transform -translate-y-1/2 opacity-0"></div>
                 {/* ESTOY QUITANDO LA LINEA */}
                 {/* LINEA */}
                 <div className="flex-column justify-between ">
@@ -200,7 +95,7 @@ const Workflow = (props: InjectedViewportProps<HTMLDivElement>) => {
                       onMouseLeave={handleMouseLeave}
                     >
                       {activeTooltip === index && (
-                        <div className="absolute left-1/2 transform -translate-x-1/2 bg-white text-black p-2 rounded-lg shadow w-[300px] h-[116px] z-10 flex items-center justify-center  ">
+                        <div className="absolute left-1/2 transform -translate-x-1/2 bg-white text-black p-2 rounded-lg w-[300px] h-[116px] z-10 flex items-center justify-center  ">
                           {tooltips[index]}
                         </div>
                       )}
@@ -225,7 +120,7 @@ const Workflow = (props: InjectedViewportProps<HTMLDivElement>) => {
     } else {
       return (
         <div className="">
-          <div className="h-[700px] bg-gradient-to-r from-blue-500 to-blue-700 py-40 justify-center sm:py-12 font-sans drop-shadow-xl">
+          <div className="h-[700px] bg-gradient-to-r from-blue-500 to-blue-700 py-40 justify-center sm:py-12 font-sans">
             <div className="text-center w-full mb-40 mt-10 text-white">
               <h1 className="text-3xl font-bold mb-4">WORKFLOW</h1>
               <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
@@ -234,7 +129,7 @@ const Workflow = (props: InjectedViewportProps<HTMLDivElement>) => {
                 applications.
               </p>
             </div>
-            <div className="w-[900px] items-center justify-center mx-auto drop-shadow-lg">
+            <div className="w-[900px] items-center justify-center mx-auto">
               <div className="relative">
                 <div className="h-2 bg-white w-full absolute top-1/2 transform -translate-y-1/2"></div>
                 <div className="flex justify-between">
