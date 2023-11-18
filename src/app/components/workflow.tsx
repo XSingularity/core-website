d92e43f8b02a9ba4b71dacb07d5499c35db20835
Douglas Márquez <douglas09marquez@gmail.com>
2023-11-17 21:21:04 -0400

    fixes

--- src/app/components/workflow.tsx
+++ src/app/components/workflow.tsx
@@ -7,6 +7,12 @@ import React, { useState, useEffect } from "react";
 import handleViewport, { type InjectedViewportProps } from 'react-in-viewport';
 
 
+const TitleComponent = ({ title }:{title:string} ) => (
+  <div className="absolute top-0 left-1/2 transform -translate-x-1/2 text-white font-bold text-center w-[200px] -mt-12">
+    {title}
+  </div>
+);
+
 const Block = (props: InjectedViewportProps<HTMLDivElement>) => {
   const { inViewport, forwardedRef } = props;
   return (
@@ -14,7 +20,7 @@ const Block = (props: InjectedViewportProps<HTMLDivElement>) => {
   );
 };
 
-const ViewportBlock = handleViewport(Block, {rootMargin: '-1.0px'});
+const ViewportBlock = handleViewport(Block, { rootMargin: '-1.0px' });
 
 const Workflow = (props: InjectedViewportProps<HTMLDivElement>) => {
   const [windowSize, setWindowSize] = useState<number>(0); // useState(window.innerWidth);
@@ -87,40 +93,42 @@ const Workflow = (props: InjectedViewportProps<HTMLDivElement>) => {
     } else {
       // Para tamaños grandes.........
       return <div className="">
-        <div className="h-[700px] bg-gradient-to-r from-blue-500 to-blue-700 py-40 justify-center sm:py-12 font-sans drop-shadow-xl">
-          <div className="text-center w-full mb-40 mt-10 text-white">
-            <h1 className="text-3xl font-bold mb-4">WORKFLOW</h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-              We offer a comprehensive range of services, from project management to
-              design and development, to ensure the success of your projects and
-              applications.
-            </p>
+      
+      <div className="h-[700px] bg-gradient-to-r from-blue-500 to-blue-700 py-40 justify-center sm:py-12 font-sans drop-shadow-xl">
+        <div className="text-center w-full mb-40 mt-10 text-white">
+          <h1 className="text-3xl font-bold mb-4">WORKFLOW</h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            We offer a comprehensive range of services, from project management to
+            design and development, to ensure the success of your projects and
+            applications.
+          </p>
           </div>
-          <div className="w-[900px] items-center justify-center mx-auto drop-shadow-lg">
-            <div className="relative">
-              <div className="h-2 bg-white w-full absolute top-1/2 transform -translate-y-1/2"></div>
-              <div className="flex justify-between">
-                {tooltips.map((text, index) => (
-                  <div
-                    key={index}
-                    className="relative hover:scale-[1.1] transition duration-1000"
-                    onMouseEnter={() => handleMouseEnter(index)}
-                    onMouseLeave={handleMouseLeave}
-                  >
-                    <div className="w-[90px] h-[90px] bg-white rounded-full cursor-pointer"></div>
-                    {activeTooltip === index && (
-                      <div className="absolute left-1/2 transform -translate-x-1/2 bg-white text-black p-2 rounded-lg shadow text-center mt-10 w-[200px]">
-                        {text}
+            <div className="w-[900px] items-center justify-center mx-auto drop-shadow-lg">
+              <div className="relative">
+                <div className="h-2 bg-white w-full absolute top-1/2 transform -translate-y-1/2"></div>
+                <div className="flex justify-between">
+                  {tooltips.map((text, index) => (
+                    <div
+                      key={index}
+                      className="relative hover:scale-[1.1] transition duration-1000"
+                      onMouseEnter={() => handleMouseEnter(index)}
+                      onMouseLeave={handleMouseLeave}
+                    >
+                      <div className="w-[90px] h-[90px] bg-white rounded-full cursor-pointer"></div>
+                      {activeTooltip === index && (
+                        <div className="absolute left-1/2 transform -translate-x-1/2 bg-white text-black p-2 rounded-lg shadow text-center mt-10 w-[200px]">
+                          {text}
+                        </div>
+                      )}
+                      <div className="absolute top-[50%] left-[50%] transform translate-y-[-50%] translate-x-[-50%] cursor-pointer">
+                        {icons[index]}
+                        <TitleComponent title={titles[index]} /> 
                       </div>
-                    )}
-                    <div className="absolute top-[50%] left-[50%] transform translate-y-[-50%] translate-x-[-50%] cursor-pointer">
-                      {icons[index]}
+                     
                     </div>
-                    <div className="absolute top-0 left-1/2 transform -translate-x-1/2 text-white font-bold text-center w-[200px] -mt-12">
-                      {titles[index]}
-                    </div>
-                  </div>
+                  
                 ))}
+                
               </div>
             </div>
           </div>
