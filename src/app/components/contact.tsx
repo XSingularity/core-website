daafda3092423621c55a94990c935391d9387b1f
Omar <omarcorporated@gmail.com>
2023-11-20 20:34:08 -0600

    token a lo loco

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -22,7 +22,7 @@ const Contact = () => {
     {
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': process.env.DO_FUNCTION_TOKEN
+        'Authorization': 'Basic NDBlYmMxMDItZmUxMC00ZjZmLTkxYzAtN2ZiYTIwMzM3ZjZhOjIxVFpSS0RYV3FveDlaMGVtWWFzT1pVQ2xTTVBTbkVsd3JFeDBUWnF6VzhzQXg4Q2pHNnA2RHRWMWNFWHZDZ3I='
       }
     }
     ).then((response: any) => {
