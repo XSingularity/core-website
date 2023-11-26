85f6468a76cb9076c902329539f847ee208ae3b7
Omar <omarcorporated@gmail.com>
2023-11-25 20:27:45 -0600

    arrow scroll color

--- src/app/components/intro.tsx
+++ src/app/components/intro.tsx
@@ -33,7 +33,7 @@ const Intro = () => {
           <div className='sm:my-0 md:my-0 lg:my-2 xl:my-10 '>
             <ul className=" pt-6 text-md market-text-lg list-disc text-gray-900">
               {/* edit puntos, tamano letra etc*/}
-              <nav className="flex text-base font-sans text-gray-600 flex-col sm:items-start sm:text-left text-center items-center">
+              <nav className="flex text-base font-sans text-gray-800 flex-col sm:items-start sm:text-left text-center items-center">
                 <a className="text-justify mb-4">
                   <span className="bg-indigo-100 text-blue-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center ">
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="w-3 h-3 " viewBox="0 0 24 24">
